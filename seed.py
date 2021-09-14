7359c20ed0917319a5d1b66dabc76bc626fbe66d
Chad Smith <chadsmithmusic@icloud.com>
2021-09-13 18:55:44 -0700

    first commit

---
+++ seed.py
@@ -0,0 +1,24 @@
+"""Seed file to make sample data for pets db."""
+
+from models import User, db
+from app import app
+
+# Create all tables
+db.drop_all()
+db.create_all()
+
+# If table isn't empty, empty it
+User.query.delete()
+
+# Add pets
+alan = User(first_name="Alan", last_name="Alda", image_url="http://www.google.com")
+joel = User(first_name="Joel", last_name="Burton", image_url="http://www.myspace.com")
+jane = User(first_name="Jane", last_name="Smith", image_url="http://www.aol.com")
+
+# Add new objects to session, so they'll persist
+db.session.add(alan)
+db.session.add(joel)
+db.session.add(jane)
+
+# Commit--otherwise, this never gets saved!
+db.session.commit()
