75b9c662061511f49f1f007b67469f65e16742d4
Chad Smith <chadsmithmusic@icloud.com>
2021-09-16 19:32:27 -0700

    added Post class

--- models.py
+++ models.py
@@ -1,12 +1,7 @@
-"""Models for Blogly."""
-
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
-
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
 
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
@@ -14,22 +9,24 @@ DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer,
-                   primary_key=True,
-                   autoincrement=True)
-
-    first_name = db.Column(db.Text,
-                     nullable=False,
-                     unique=True)
-
-    last_name = db.Column(db.Text,
-                        nullable=False,
-                        unique=True)
-
+    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    first_name = db.Column(db.Text, nullable=False, unique=True)
+    last_name = db.Column(db.Text, nullable=False, unique=True)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     @property
     def full_name(self):
         """Return full name of user."""
 
         return f"{self.first_name} {self.last_name}"
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
