f151188b99e7708bf61f82b13084944ad09ba95b
Chad Smith <chadsmithmusic@icloud.com>
2021-09-21 19:18:15 -0700

    added tag routes

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = "secret123"
-debug = DebugToolbarExtension(app)
+app.config['SECRET_KEY'] = 'ihaveasecret'
 
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
 
+
 @app.route('/')
 def root():
     """Show recent list of posts, most-recent first."""
+
     posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
     return render_template("posts/homepage.html", posts=posts)
 
@@ -26,27 +32,40 @@ def page_not_found(e):
 
     return render_template('404.html'), 404
 
-@app.route('/users', methods=["GET"])
-def list_users():
-    """Shows list of all users in db"""
-    users = User.query.all()
+
+##############################################################################
+# User route
+
+@app.route('/users')
+def users_index():
+    """Show a page with info on all users"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
     return render_template('users/index.html', users=users)
 
-@app.route('/users/new')
-def show_form():
+
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
+
     return render_template('users/new.html')
 
-@app.route('/users/new', methods=["POST"])
-def create_new_user():
+
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Handle form submission for creating a new user"""
+
     new_user = User(
-    first_name = request.form["first_name"],
-    last_name = request.form["last_name"],
-    image_url = request.form["image_url"] or None)
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
 
     db.session.add(new_user)
     db.session.commit()
+    flash(f"User {new_user.full_name} added.")
 
-    return redirect('/users')
+    return redirect("/users")
+
 
 @app.route('/users/<int:user_id>')
 def users_show(user_id):
@@ -54,6 +73,7 @@ def users_show(user_id):
 
     user = User.query.get_or_404(user_id)
     return render_template('users/show.html', user=user)
+
 
 @app.route('/users/<int:user_id>/edit')
 def users_edit(user_id):
@@ -74,6 +94,7 @@ def users_update(user_id):
 
     db.session.add(user)
     db.session.commit()
+    flash(f"User {user.full_name} edited.")
 
     return redirect("/users")
 
@@ -85,15 +106,22 @@ def users_destroy(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
+    flash(f"User {user.full_name} deleted.")
 
     return redirect("/users")
 
-#################### PART 2 ######################
+
+##############################################################################
+# Posts route
+
 
 @app.route('/users/<int:user_id>/posts/new')
-def new_user_posts(user_id):
+def posts_new_form(user_id):
+    """Show a form to create a new post for a specific user"""
+
     user = User.query.get_or_404(user_id)
-    return render_template('posts/new.html', user=user)
+    tags = Tag.query.all()
+    return render_template('posts/new.html', user=user, tags=tags)
 
 
 @app.route('/users/<int:user_id>/posts/new', methods=["POST"])
@@ -101,26 +129,36 @@ def posts_new(user_id):
     """Handle form submission for creating a new post for a specific user"""
 
     user = User.query.get_or_404(user_id)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
     new_post = Post(title=request.form['title'],
                     content=request.form['content'],
-                    user=user)
+                    user=user,
+                    tags=tags)
 
     db.session.add(new_post)
     db.session.commit()
+    flash(f"Post '{new_post.title}' added.")
 
     return redirect(f"/users/{user_id}")
 
+
 @app.route('/posts/<int:post_id>')
 def posts_show(post_id):
-    """Show a post. Show buttons to edit and delete the post."""
+    """Show a page with info on a specific post"""
+
     post = Post.query.get_or_404(post_id)
-    return render_template('/posts/show.html', post=post)
+    return render_template('posts/show.html', post=post)
+
 
 @app.route('/posts/<int:post_id>/edit')
 def posts_edit(post_id):
-    '''Show form to edit a post, and to cancel (back to user page).'''
+    """Show a form to edit an existing post"""
+
     post = Post.query.get_or_404(post_id)
-    return render_template('/posts/edit.html', post=post)
+    tags = Tag.query.all()
+    return render_template('posts/edit.html', post=post, tags=tags)
 
 
 @app.route('/posts/<int:post_id>/edit', methods=["POST"])
@@ -131,8 +169,12 @@ def posts_update(post_id):
     post.title = request.form['title']
     post.content = request.form['content']
 
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
+
     db.session.add(post)
     db.session.commit()
+    flash(f"Post '{post.title}' edited.")
 
     return redirect(f"/users/{post.user_id}")
 
@@ -145,5 +187,86 @@ def posts_destroy(post_id):
 
     db.session.delete(post)
     db.session.commit()
+    flash(f"Post '{post.title} deleted.")
 
     return redirect(f"/users/{post.user_id}")
+
+
+##############################################################################
+# Tags route
+
+
+@app.route('/tags')
+def tags_index():
+    """Show a page with info on all tags"""
+
+    tags = Tag.query.all()
+    return render_template('tags/index.html', tags=tags)
+
+
+@app.route('/tags/new')
+def tags_new_form():
+    """Show a form to create a new tag"""
+
+    posts = Post.query.all()
+    return render_template('tags/new.html', posts=posts)
+
+
+@app.route("/tags/new", methods=["POST"])
+def tags_new():
+    """Handle form submission for creating a new tag"""
+
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    new_tag = Tag(name=request.form['name'], posts=posts)
+
+    db.session.add(new_tag)
+    db.session.commit()
+    flash(f"Tag '{new_tag.name}' added.")
+
+    return redirect("/tags")
+
+
+@app.route('/tags/<int:tag_id>')
+def tags_show(tag_id):
+    """Show a page with info on a specific tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tags/show.html', tag=tag)
+
+
+@app.route('/tags/<int:tag_id>/edit')
+def tags_edit_form(tag_id):
+    """Show a form to edit an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    posts = Post.query.all()
+    return render_template('tags/edit.html', tag=tag, posts=posts)
+
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def tags_edit(tag_id):
+    """Handle form submission for updating an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    tag.name = request.form['name']
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+
+    db.session.add(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' edited.")
+
+    return redirect("/tags")
+
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def tags_destroy(tag_id):
+    """Handle form submission for deleting an existing tag"""
+
+    tag = Tag.query.get_or_404(tag_id)
+    db.session.delete(tag)
+    db.session.commit()
+    flash(f"Tag '{tag.name}' deleted.")
+
+    return redirect("/tags")
--- models.py
+++ models.py
@@ -1,20 +1,23 @@
-from flask_sqlalchemy import SQLAlchemy
 import datetime
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
 
-# MODELS GO BELOW!
-class User(db.Model):
-    __tablename__ = 'users'
 
-    id = db.Column(db.Integer,primary_key=True)
+class User(db.Model):
+    """Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.Text, nullable=False)
     last_name = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
-    posts = db.relationship("Post", backref="user", cascade="all, delete-orphan")
+    posts = db.relationship("Post", backref="user",
+                            cascade="all, delete-orphan")
 
     @property
     def full_name(self):
@@ -22,11 +25,19 @@ class User(db.Model):
 
         return f"{self.first_name} {self.last_name}"
 
+
 class Post(db.Model):
+    """Blog post."""
+
+    __tablename__ = "posts"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.Text, nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     @property
@@ -35,6 +46,38 @@ class Post(db.Model):
 
         return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
 
+
+class PostTag(db.Model):
+    """Tag on a post."""
+
+    __tablename__ = "posts_tags"
+
+    post_id = db.Column(db.Integer, db.ForeignKey(
+        'posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class Tag(db.Model):
+    """Tag that can be added to posts."""
+
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship(
+        'Post',
+        secondary="posts_tags",
+        # cascade="all,delete",
+        backref="tags",
+    )
+
+
 def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
     db.app = app
     db.init_app(app)
